dcd001f3c2f28012046bbd515d6dc2583a6eb272
spacewaffles5827 <jackhartwig7@gmail.com>
2022-10-19 01:16:44 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, React } from 'react';
+
 
 function App() {
+
+  const [ boardData, setBoardData ] = useState([null, null, null, null, null, null, null, null, null]);
+  const [ player, setPlayer ] = useState('X');
+  const updateBoard = (index) => {
+    const newBoardData = [...boardData];
+    newBoardData[index] = player;
+    setBoardData(newBoardData);
+    switchPlayer();
+  }
+
+  const switchPlayer = () => {
+    if (player === 'X') {
+      setPlayer('O');
+    } else {
+      setPlayer('X');
+    }
+  }
+
+  function checkWinner() {
+    const winningCombos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    for (let i = 0; i < winningCombos.length; i++) {
+      const [a, b, c] = winningCombos[i];
+      if (boardData[a] && boardData[a] === boardData[b] && boardData[a] === boardData[c]) {
+        console.log('winner:', boardData[a]);
+        return boardData[a];
+      }
+    }
+    console.log('no winner');
+    return null;
+  }
+
+  function Tile({value, index}){
+    return(
+      <div className="Tile" onClick={() => updateBoard(index)}>
+        <h1>{value}</h1>
+      </div>
+    )
+  }
+  
+  function Board(){
+    return(
+      <div className="Board">
+        {
+          boardData.map((tile, index) => {
+            return <Tile key={index} value={tile} index={index}></Tile>
+          })
+        }
+      </div>
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {checkWinner()}
+      <Board></Board>
+      <h1>Turn: {player}</h1>
     </div>
   );
 }
